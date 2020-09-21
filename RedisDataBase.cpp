f749c8bf04976d5b53e233a790fa482e836460ff
root <root@LAPTOP-O772QOCH.localdomain>
2020-09-21 16:58:01 +0800

    增加了注册，验证，和删除用户功能

--- RedisDataBase.cpp
+++ RedisDataBase.cpp
@@ -1,10 +1,12 @@
 #include "RedisDataBase.h"
 #include <iostream>
+#include <cstring>
+#include <string>
 
 
 RedisDataBase::RedisDataBase(const char *redis_IP = "127.0.0.1", int redis_port = 6379)
 {
-    this->redisConnectRet = redisConnect(const char *redis_IP, int redis_port);
+    this->redisConnectRet = redisConnectWithTimeout(const char *redis_IP, int redis_port);
     if (redisConnectRet == NULL || redisConnectRet->err)  // redisContext is not thread-safe
     {
         if (redisConnectRet) 
@@ -23,4 +25,58 @@ RedisDataBase::RedisDataBase(const char *redis_IP = "127.0.0.1", int redis_port 
 RedisDataBase::~RedisDataBase)()
 {
     void redisFree(this->redisConnectRet);
+}
+
+bool RedisDataBase::RegisterUser(const UserInfo &user_info)
+{
+    string registerCommand;
+    registerCommand = "SET " + user_info.userName + " " + user_info.userPassword;
+    void *ret = redisCommand(this->redisConnectRet, registerCommand.c_str());
+    
+    if (!ret)   // 注册失败，返回NULL
+    {
+        std::cout << "Fail to register the user" << std::endl;
+        return false;
+    }
+
+    std::cout << "Successfully register the user" << std::endl;
+    return true;
+}
+
+bool RedisDataBase::VerifyUser(UserInfo user_info)
+{
+    string verifyCommand;
+    verifyCommand = "GET " + user_info.userName + " " + user_info.userPassword;
+    void *reply = redisCommand(this->redisConnectRet, verifyCommand.c_str());
+    if (reply->type == REDIS_REPLY_NIL) // 若用户名不存在
+    {
+        std::cout << "The user does not exist" << std::endl;
+        return false;
+    }
+        
+    // 用户名存在
+    return true;
+}
+
+bool RedisDataBase::DeleteUser(UserInfo user_info)
+{
+    string deleteCommand;
+    deleteCommand = "DEL " + user_info.userName + " " + user_info.userPassword;
+    void *reply = redisCommand(this->redisConnectRet, deleteCommand.c_str());
+    // 如果用户名不存在则忽略
+       
+    if (reply->integer > 0) // 用户名存在，返回删除的键的长度
+    {
+        std::cout << "The user has been deleted" << std::endl;
+        return true;
+    }
+    else
+    {
+        throw std::exception();
+    }
+}
+
+bool RedisDataBase::CleanInactiveUser()
+{
+    
 }
--- RedisDataBase.h
+++ RedisDataBase.h
@@ -2,11 +2,26 @@
 #define REDISDATABASE_H
 #include <hiredis/hiredis.h>
 
+/*
+    使用Redis存储用户名和密码
+    实现了注册，验证，删除用户功能
+    清理不活跃用户
+*/
 class RedisDataBase
 {
 public:
     RedisDataBase();
     ~RedisDataBase();
+    typedef struct UserInfo
+    {
+        string userName;
+        string userPassword;
+    };
+
+    bool RegisterUser(UserInfo user_info);
+    bool DeleteUser(UserInfo user_info);
+    bool VerifyUser(UserInfo user_info);
+    bool CleanInactiveUser();
 
 private:
     redisContext *redisConnectRet;
