a49eeecc7fc8dccafb73f576801a4ce7e11eba90
root <root@LAPTOP-O772QOCH.localdomain>
2020-09-23 21:19:49 +0800

    Version 1.0

---
+++ TimerLinkedList.h
@@ -0,0 +1,22 @@
+#ifndef TIMERLINKEDLIST_H
+#define TIMERLINKEDLIST_H
+
+class TimerLinkedList
+{
+private:
+    /* data */
+public:
+    TimerLinkedList(/* args */);
+    ~TimerLinkedList();
+};
+
+TimerLinkedList::TimerLinkedList(/* args */)
+{
+}
+
+TimerLinkedList::~TimerLinkedList()
+{
+}
+
+
+#endif
