639be66730279a763cb93c2f85ac74be0952525c
ColbertBrave <colbert.ran@outlook.com>
2020-11-19 19:39:20 +0800

    add write an read

--- Utils.cpp
+++ Utils.cpp
@@ -1,5 +1,7 @@
 #include "Utils.h"
 #include "string.h"
+#include <unistd.h>
+const ssize_t MAX_BUFF = 4096;
 
 bool AddFd(int epoll_fd, int fd)
 {
@@ -46,7 +48,7 @@ int SetListenFd(int port)
     if (port > 65535 && port < 1024)
     {
         LOG << "Invalid server port\n";    // TODO 改为写入到日志中 DOING 未测试
-        throw std::exception()
+        throw std::exception();
     }
 
     struct sockaddr_in *server_addr;
@@ -67,4 +69,74 @@ int SetListenFd(int port)
         throw std::exception();
     }
     return listen_fd;
+}
+
+ssize_t ReadFromBuffer(int fd, std::string &buffer)
+{
+    // 循环读取缓冲区的内容，直到读取完毕
+    ssize_t sumReadBytes = 0;
+    char tempBuffer[MAX_BUFF];
+    while (true)
+    {
+        ssize_t onceReadBytes = read(fd, tempBuffer, MAX_BUFF);
+        if (onceReadBytes == 0)
+        {
+            return sumReadBytes;
+        }
+
+        if (onceReadBytes < 0) && (errno == EINTR)
+        {
+            continue;
+        }
+
+        if (onceReadBytes < 0) && (errno == EAGAIN)
+        {
+            return sumReadBytes;
+        }
+
+        if (onceReadBytes < 0)
+        {
+            LOG << "Failed to read data from fd\n";
+            return -1;
+        }
+
+        sumReadBytes += onceReadBytes;
+        buffer += std::string(tempBuffer, tempBuffer + onceReadBytes);
+    }
+}
+
+// TODO 读和写的关系似乎混淆了, 还有缓冲区似乎也有些问题
+ssize_t WriteToBuffer(int fd, std::string &buffer)
+{
+    ssize_t alreadyWritten = 0;
+    ssize_t leftBytes = buffer.size();
+    const char *writeBuf = buffer.c_str();
+    while (true)
+    {
+        ssize_t onceReadBytes = write(fd, write, leftBytes);
+        if (leftBytes == 0)
+        {
+            return; // TODO
+        }
+
+        if (onceReadBytes < 0) && (errno == EINTR)
+        {
+            continue;
+        }
+
+        if (onceReadBytes < 0) && (errno == EAGAIN)
+        {
+            break;
+        }
+
+        if (onceReadBytes < 0)
+        {
+            return -1;
+        }
+
+        alreadyWritten += onceReadBytes;
+        leftBytes -= onceReadBytes;
+        writeBuf += onceReadBytes;
+    }
+    return alreadyWritten;
 }
--- Utils.h
+++ Utils.h
@@ -10,4 +10,10 @@ bool RemoveFd(int epoll_fd, int fd);
 bool SetSocketNonBlocking(int fd);
 int SetListenFd(int port = 8888);
 
+// 从fd的缓冲区中读取内容到buffer中
+ssize_t ReadFromBuffer(int fd, std::string &buffer);
+
+// 将buffer的内容写至fd的缓冲区中
+ssize_t WriteToBuffer(int fd, std::string &buffer);
+
 #endif
