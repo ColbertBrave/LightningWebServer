6bc1a6989776d4a1356e1dd1213bf3adaa332949
ColbertBrave <colbert.ran@outlook.com>
2020-11-22 14:33:16 +0800

    add some functions

--- Timer/Timer.cpp
+++ Timer/Timer.cpp
@@ -1,21 +1,69 @@
 #include "Timer.h"
+#include <sys/time.h>
 
-TimerNode::TimerNode()
+TimerNode::TimerNode(std::shared_ptr<HttpRequest> request, int timeout): Request(request)
 {
-
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    this->ExpiredTime = (((now.tv_sec % 10000) * 1000) + (now.tv_usec / 1000)) + timeout;
+    Status = TimerNodeStatus::VALID;
 }
 
 TimerNode::~TimerNode() 
 {
-
+    Request->CloseHttp();
 }
 
-TimerQueue::TimerQueue()
+// 更新节点的超时时间
+void TimerNode::UpateTimeNode(int timeout)
 {
-
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    this->ExpiredTime = (((now.tv_sec % 10000) * 1000) + (now.tv_usec / 1000)) + timeout;
 }
 
-TimerQueue::~TimerQueue()
+// 获取节点的过期时间
+size_t TimerNode::GetExpiredTime()
 {
-    
+    return this->ExpiredTime;
+}
+
+// 检查节点的超时时间是否已经到达
+bool TimerNode::CheckStatus()
+{
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    size_t temp = (((now.tv_sec % 10000) * 1000) + (now.tv_usec / 1000));
+    if (temp < ExpiredTime)
+        return true;
+    else 
+    {
+        Status = TimerNodeStatus::EXPIRED;
+        return false;
+    }
+}
+
+TimerQueue::TimerQueue() {}
+
+TimerQueue::~TimerQueue() {}
+
+void TimerQueue::AddTimerNode(TimerNode &node)
+{
+    TimeNodePtr NodePtr = std::make_shared<TimerNode>(node);
+    TimerPriorityQueue.push(NodePtr);
+}
+
+void TimerQueue::RemoveExpiredTimerNode()
+{
+    while (!TimerPriorityQueu.empty())
+    {
+        TimeNodePtr NodePtr = TimerPriorityQueue.top();
+        if (NodePtr->CheckStatus())
+        {
+            // 若尚未超时，节点有效
+            return;
+        }
+
+        TimerPriorityQueue.pop();
+    }
 }
--- Timer/Timer.h
+++ Timer/Timer.h
@@ -1,22 +1,55 @@
 #ifndef TIMER_H
 #define TIMER_H
-
+#include <memory>
+#include <queue>
+#include "../HttpRequest.h"
 
 class TimerNode
 {
 private:
-    
+    enum class TimerNodeStatus
+    {
+        VALID;
+        EXPIRED
+    };
+    std::shared_ptr<HttpRequest> Request;
+    size_t ExpiredTime;
+
 public:
-    TimerNode(/* args */);
+    TimerNode(std::shared_ptr<HttpRequest> request, int timeout);
     ~TimerNode();
+
+    void                UpateTimeNode(int timeout);
+    size_t              GetExpiredTime();
+    bool                CheckStatus();
+    TimerNodeStatus     Status;
 };
+
+// 仿照std::greater定义了一个函数对象，用于TimerQueue中priority_queue不同TimerNode的比较
+struct GreaterTimer
+{
+    bool operator()(TimerNode& timer_node1, TimerNode& timer_node2) const
+    {
+        if (timer_node1.GetExpiredTime() > timer_node2.GetExpiredTime())
+            return timer_node1.GetExpiredTime();
+        else
+            return timer_node2.GetExpiredTime();
+    }
+};
+
 
 class TimerQueue
 {
 private:
+    typedef std::shared_ptr<TimerNode> TimerNodePtr;
+    // 升序队列，小根堆
+    std::priority_queue<TimerNodePtr, std::deque<TimerNodePtr>, GreaterTimer> TimerPriorityQueue;
 
 public:
     TimerQueue();
     ~TimerQueue();
+
+    void AddTimerNode(TimerNode &node);
+    void RemoveExpiredTimerNode();
 }
 #endif
